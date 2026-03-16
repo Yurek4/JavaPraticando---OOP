b5c7e9e1a410ca457338917ad2f4847d552525ad
Yuri Dev <147882154+Yurek4@users.noreply.github.com>
2026-03-16 17:09:46 -0300

    Create GameLauncher class to start GuessGame

---
+++ GameLaucher.java
@@ -0,0 +1,11 @@
+package NewProjeto;
+
+
+public class GameLauncher {
+		public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			GuessGame Jogo1 = new GuessGame();
+			Jogo1.startGame();
+	}
+
+}
